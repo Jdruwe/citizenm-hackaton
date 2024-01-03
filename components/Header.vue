79d42ae5837eec266a74d06d0216c0f5101b0c05
Jeroen Druwe <jeroen.druwe@signify.com>
2024-01-03 23:15:00 +0100

    Working on the header

--- components/Header.vue
+++ components/Header.vue
@@ -42,24 +42,45 @@ onMounted(() => {
       </div>
     </div>
     <div id="mega-menu-full-dropdown" class="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600 hidden">
-      <ul class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:grid-cols-4 md:px-6">
-        <li>
-          <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-            <div class="font-semibold">Online Stores</div>
-            <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-          </a>
+      <ul class="max-w-screen-xl px-6 py-5 mx-auto text-gray-900 dark:text-white">
+        <li class="flex flex-col mt-3">
+          <p class="text-sm text-gray-500 border-b border-gray-400 pb-1 self-start">
+            Europa
+          </p>
+          <ul class="grid grid-cols-2 md:grid-cols-4 mt-5 gap-2">
+            <li>
+              <a href="#" class="block rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                <div class="text-sm font-semibold">Amsterdam</div>
+                <p class="text-sm text-gray-500 dark:text-gray-400">Amstel Amsterdam</p>
+                <p class="text-sm text-gray-500 dark:text-gray-400">Amsterdam Zuid</p>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="block rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                <div class="text-sm font-semibold">Kopenhagen</div>
+                <p class="text-sm text-gray-500 dark:text-gray-400">Kopenhagen</p>
+              </a>
+            </li>
+          </ul>
         </li>
-        <li>
-          <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-            <div class="font-semibold">Segmentation</div>
-            <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-            <div class="font-semibold">Marketing CRM</div>
-            <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-          </a>
+        <li class="flex flex-col mt-10">
+          <p class="text-sm text-gray-500 border-b border-gray-400 pb-1 self-start">
+            Amerika
+          </p>
+          <ul class="grid grid-cols-2 md:grid-cols-4 mt-5">
+            <li>
+              <a href="#" class="block rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                <div class="text-sm font-semibold">Austin</div>
+                <p class="text-sm text-gray-500 dark:text-gray-400">Austin Downtown</p>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="block rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                <div class="text-sm font-semibold">Boston</div>
+                <p class="text-sm text-gray-500 dark:text-gray-400">Boston North Station</p>
+              </a>
+            </li>
+          </ul>
         </li>
       </ul>
     </div>
