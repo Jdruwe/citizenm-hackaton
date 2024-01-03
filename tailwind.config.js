63e1f7093be72f9b451a24c5bf94b829a122b701
Jeroen Druwe <jeroen.druwe@signify.com>
2024-01-03 13:52:56 +0100

    Added flowbite-vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ export default {
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
-    './node_modules/flowbite/**/*.{js,ts}',
+    './node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
