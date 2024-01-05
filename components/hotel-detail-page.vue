316613c6cbf7f70a0875348dd18bc2353bb7f0dd
Jeroen Druwe <jeroen.druwe@signify.com>
2024-01-05 09:20:56 +0100

    Added error page

--- components/hotel-detail-page.vue
+++ components/hotel-detail-page.vue
@@ -7,32 +7,32 @@ interface Props {
 
 const { slug } = defineProps<Props>()
 
-const { data: hotel } = await useAsyncData('hotel', () => {
+const { data } = await useAsyncData('hotel', () => {
   const hotelService = new HotelService()
   return hotelService.getHotel(slug)
 })
 
-if (!hotel.value) {
+if (!data.value) {
   throw createError({
     statusCode: 404,
-    statusMessage: 'Page Not Found',
+    statusMessage: 'Deze pagina bestaat niet (meer).',
   })
 }
 </script>
 
 <template>
-  <section class="bg-white dark:bg-gray-900">
+  <section v-if="data" class="bg-white dark:bg-gray-900">
     <div class="grid mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
       <div class="mr-auto place-self-center lg:col-span-7">
         <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-          {{ hotel?.name }}
+          {{ data?.name }}
         </h1>
         <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-          {{ hotel?.shortDescription }}
+          {{ data?.shortDescription }}
         </p>
       </div>
       <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-        <NuxtImg provider="contentful" :src="hotel?.mainImage" alt="" width="600" height="600" />
+        <NuxtImg provider="contentful" :src="data?.mainImage" alt="" width="600" height="600" />
       </div>
     </div>
   </section>
---
+++ error.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Props {
+  error: {
+    statusCode: number
+    statusMessage: string
+  }
+}
+
+const { error } = defineProps<Props>()
+</script>
+
+<template>
+  <NuxtLayout>
+    <section>
+      <h1 class="mb-4 text-4xl tracking-tight font-extrabold text-primary-600 dark:text-primary-500 mt-10">
+        error {{ error.statusCode }}
+      </h1>
+      <p class="mb-4 text-lg font-light text-gray-700">
+        {{ error.statusMessage }}
+      </p>
+      <NuxtLink to="/" class="inline-block bg-red-600 text-white rounded-md p-4">
+        home
+      </NuxtLink>
+    </section>
+  </NuxtLayout>
+</template>
