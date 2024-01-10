cc05b7c4631a038226b9dedd8d87a71405eb7ccd
Jeroen Druwe <jeroen.druwe@signify.com>
2024-01-10 09:58:23 +0100

    Cleaned up slider

--- components/slider.vue
+++ components/slider.vue
@@ -28,17 +28,24 @@ const modules = [
   Navigation,
 ]
 
+const swiper = reactive({
+  instance: null as typeof Swiper | null,
+})
+
 function setSwiperReference(instance: typeof Swiper) {
-  swiperInstance = instance
+  swiper.instance = instance
 }
 
 function showPreviousSlide() {
-  swiperInstance?.slidePrev()
+  swiper.instance?.slidePrev()
 }
 
 function showNextSlide() {
-  swiperInstance?.slideNext()
+  swiper.instance?.slideNext()
 }
+
+const isBeginning = computed(() => swiper.instance?.isBeginning)
+const isEnd = computed(() => swiper.instance?.isEnd)
 </script>
 
 <template>
@@ -46,8 +53,8 @@ function showNextSlide() {
     {{ title }}
   </h2>
   <div class="flex">
-    <button class="mr-10" @click="showPreviousSlide()">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <button class="mr-10 group" :disabled="isBeginning" @click="showPreviousSlide()">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 group-disabled:text-gray-400">
         <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
       </svg>
     </button>
@@ -58,7 +65,6 @@ function showNextSlide() {
       :modules="modules"
       :breakpoints="breakpoints"
       :free-mode="true"
-      :loop="true"
       @swiper="setSwiperReference"
     >
       <SwiperSlide v-for="item in filteredItems" :key="item.id">
@@ -82,8 +88,8 @@ function showNextSlide() {
         </div>
       </SwiperSlide>
     </Swiper>
-    <button class="ml-10" @click="showNextSlide()">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <button class="ml-10 group" :disabled="isEnd" @click="showNextSlide()">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 group-disabled:text-gray-400">
         <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
       </svg>
     </button>
