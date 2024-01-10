724f8468c4a229dafa259fd3162da9204c725299
Jeroen Druwe <jeroen.druwe@signify.com>
2024-01-10 15:43:57 +0100

    Fixed header translations

--- components/header.vue
+++ components/header.vue
@@ -87,7 +87,7 @@ function sanitize(value: string) {
               id="mega-menu-full-dropdown-button" data-collapse-toggle="mega-menu-full-dropdown"
               class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded md:w-auto hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-600 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-red-500 md:dark:hover:bg-transparent dark:border-gray-700 font-extrabold"
             >
-              bekijk hotels per stad
+              browse hotels by city
               <svg
                 class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                 viewBox="0 0 10 6"
@@ -103,19 +103,19 @@ function sanitize(value: string) {
             <a
               href="#"
               class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-red-500 md:dark:hover:bg-transparent dark:border-gray-700 font-extrabold"
-            >vergaderingen</a>
+            >meetings</a>
           </li>
           <li>
             <a
               href="#"
               class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-red-500 md:dark:hover:bg-transparent dark:border-gray-700 font-extrabold"
-            >lidmaatschap</a>
+            >membership</a>
           </li>
           <li>
             <a
               href="#"
               class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-red-500 md:dark:hover:bg-transparent dark:border-gray-700 font-extrabold"
-            >groepen</a>
+            >groups</a>
           </li>
         </ul>
       </div>
